3813cd288626ec311ac3020c484079a7cc9aca8a
Saima223 <siddiqui22062004@gmail.com>
2026-07-15 23:58:44 +0530

    cpp intro

---
+++ c++/week1/input.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //g++ filename.cpp -o filename
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Welcome " << name;
+
+    cout << "Name: Rahul" << endl;
+    cout << "Age: 20" << endl;
+    cout << "City: Mumbai";
+
+
+    return 0;
+}
